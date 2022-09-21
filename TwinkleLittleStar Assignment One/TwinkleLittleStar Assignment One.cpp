a9cd0222461caf307c468a166cf8089479f1fab6
Josh Buker <joshbuker04@gmail.com>
2022-09-21 13:47:03 -0700

    First Assignemt Finished

---
+++ TwinkleLittleStar Assignment One/TwinkleLittleStar Assignment One.cpp
@@ -0,0 +1,54 @@
+// ==========================================================================================
+// TwinkleLittleStar Assignment One.cpp: Defines the entry point for the console application 
+//
+// Computer Scientist: Joshua Buker
+// Program Name: TwinkleLittleStar Assignemnt One
+// Version: 1.0.0
+// Date: 21 September 2022
+//
+// Inputs: Nothing
+// Outputs: The lyrics to a song
+// Algorithm: Simple sequencial algorithm to output text
+// ========================================================
+
+#include <iostream>
+
+// =================
+
+	int main() {
+
+	  // Verse One
+		std::cout << "Twinkle, twinkle, little star" << std::endl;
+		std::cout << "How I wonder what you are!" << std::endl;
+		std::cout << "Up above the world so high," << std::endl;
+		std::cout << "Like a diamind in the sky!\n" << std::endl;
+
+	  // Verse Two
+		std::cout << "When the blazing sun is gone," << std::endl;
+		std::cout << "When he nothing shines upon," << std::endl;
+		std::cout << "When you show your litte light," << std::endl;
+		std::cout << "Twinkle, twinkle, all the night.\n" << std::endl;
+	  // Verse Three
+		std::cout << "Then the traveller in the dark" << std::endl;
+		std::cout << "Thanks you for your tiny sparks;" << std::endl;
+		std::cout << "He could not see which way to go," << std::endl;
+		std::cout << "If you did not twinkle so.\n" << std::endl;
+
+	  // Verse Four
+		std::cout << "In the dark blue sky you keep," << std::endl;
+		std::cout << "And often through my curtains peep," << std::endl;
+		std::cout << "For you never shut your eye" << std::endl;
+		std::cout << "'Till the sun is in the sky." << std::endl;
+
+	  // Verse Five
+		std::cout << "As your bright and tiny spark" << std::endl;
+		std::cout << "Lights the traveller in the dark," << std::endl;
+		std::cout << "Though I know not what you are," << std::endl;
+		std::cout << "Twinkle, twinkle, little star\n" << std::endl;
+
+		return 0;
+
+	} // Function main()
+// ==========================================
+
+
